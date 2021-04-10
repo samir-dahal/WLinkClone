0e7a47bfd22aa5be86939150cca67906ec438a5a
samir <samir.dahal@crystalsolution.com.np>
2021-04-10 21:26:59 +0545

    Added billing page

---
+++ WLinkClone/WLinkClone/Models/Transaction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WLinkClone.Models
+{
+    public class Transaction
+    {
+        public string Date { get; set; }
+        public string Title { get; set; }
+        public string Number { get; set; }
+        public decimal PaidAmount { get; set; }
+    }
+}
